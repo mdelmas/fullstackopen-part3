7bc84aa095e2d07a8bd4fc499bffe9bb1c412a8e
Morgane Delmas <mdelmas93@gmail.com>
2023-09-07 12:52:42 +0200

    Part 3: exercises 3.7 + 3.8

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
 app.use(express.json());
+
+// app.use(morgan('tiny'));
+
+morgan.token('content', (request, result) => JSON.stringify(request.body))
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'));
 
 let persons = [
     { 
